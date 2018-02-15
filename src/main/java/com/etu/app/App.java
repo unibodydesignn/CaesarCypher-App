c3a01988aac85bc607f30633e4a4452889f20527
unibodydesignn <unibodydesignn@gmail.com>
2018-02-15 18:19:34 +0300

    Getting ready for deployment to heroku.

--- src/main/java/com/etu/app/App.java
+++ src/main/java/com/etu/app/App.java
@@ -1,17 +1,84 @@
 package com.etu.app;
 
-/**
- * Hello world!
- *
- */
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 public class App
 {
-    public static void main( String[] args ) {
+    public static boolean search(ArrayList<Integer> array, int e) {
+      System.out.println("inside search");
+      if (array == null) return false;
 
-        System.out.println( "Hello World!" );
-
+      for (int elt : array) {
+        if (elt == e) return true;
+      }
+      return false;
     }
 
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
+          String input1 = req.queryParams("input1");
+          java.util.Scanner sc1 = new java.util.Scanner(input1);
+          sc1.useDelimiter("[;\r\n]+");
+          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+          while (sc1.hasNext())
+          {
+            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+            inputList.add(value);
+          }
+          System.out.println(inputList);
+
+
+          String input2 = req.queryParams("input2").replaceAll("\\s","");
+          int input2AsInt = Integer.parseInt(input2);
+
+          boolean result = App.search(inputList, input2AsInt);
+
+         Map map = new HashMap();
+          map.put("result", result);
+          return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+            (rq, rs) -> {
+              Map map = new HashMap();
+              map.put("result", "not computed yet!");
+              return new ModelAndView(map, "compute.mustache");
+            },
+            new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
+
+
+
+
+
+
+/*
     public static boolean cryptionWords(Integer[] cryptCount, Integer[] lastDigit, String[] wordList, String[] crypted) {
 
         if(cryptCount.length == 0 || lastDigit.length == 0 || wordList.length == 0)
@@ -52,6 +119,7 @@ public class App
 
             return true;
         }
+        */
 
 
 
@@ -71,5 +139,4 @@ public class App
 
 
 
-    }
-}
+    
